e1f17c4411ec128536ec3265cc65e2947d8da9d5
Derixithy <derixithy@users.noreply.github.com>
2025-06-17 10:19:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ ln -s /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 
 
 # Install Apps
-dnf5 -y install gnome-tweaks gnome-extensions-app unrar nmap adb
+dnf5 -y install gnome-tweaks gnome-extensions-app unrar nmap adb waydroid
 
 # Install Gnome Extensions
 dnf5 -y install gnome-shell-extension-appindicator \
